ab3215f0002129baf96b504373de4942ff4a2d66
unknown <felipeacostacoronado@gmail.com>
2022-07-24 22:29:07 -0500

    Avances del fin de semana

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [
-  ]
+ 
 })
-export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+export class PorCapitalComponent {
 
-  ngOnInit(): void {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private PaisService: PaisService) {
+
+  }
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.PaisService.buscarCapital(this.termino).subscribe((paises) => {
+      console.log(paises);
+      this.paises = paises
+    }, (err) => { this.hayError = true; this.paises = [] });
   }
 
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,15 +8,29 @@ import { PaisService } from '../../services/pais.service';
   styles: [
   ]
 })
+
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor(private PaisService: PaisService) { }
+  constructor(private PaisService: PaisService) {
 
-  buscar() {
-    console.log(this.termino);
-    this.PaisService.buscarPais(this.termino).subscribe(resp => { console.log(resp) });
   }
 
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.PaisService.buscarPais(this.termino).subscribe((paises) => {
+      console.log(paises);
+      this.paises = paises
+    }, (err) => { this.hayError = true; this.paises = [] });
+  }
+  
+  sugerencias( termino: string ){
+    this.hayError = false;
+    //Todo crear sugerencias
+  }
 }
